dfc200451da2123ade3a0b5aaa9ab19dbdc41073
Elizabeth Stansberry <estansberry20@stuy.edu>
2021-01-19 19:18:14 -0700

    setup

--- TravelersProblem.java
+++ TravelersProblem.java
@@ -4,11 +4,7 @@ import java.lang.*;
 public class TravelersProblem {
 
   public static void main(String[] args) {
-    Scanner linescan = new Scanner(System.in);
-    String inputtext;
-    String wordtext;
-    while(linescan.hasNextLine()){
-    }
+    
   }
 
 }
