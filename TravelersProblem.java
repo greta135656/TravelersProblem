bb89508acdff95db86bdda0dd90b5bf0a849c16b
Grace <gchen20@stuy.edu>
2021-01-19 22:09:34 -0500

    Finished Scanner part to create an arraylist of cities

--- TravelersProblem.java
+++ TravelersProblem.java
@@ -4,7 +4,24 @@ import java.lang.*;
 public class TravelersProblem {
 
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
+    ArrayList<String> cities = new ArrayList<String>(10);
+
+    while (sc.hasNext()) {
+      String line = sc.nextLine();
+      Scanner lineSc = new Scanner(line);
+
+      String city = lineSc.next();
+      if (! cities.contains(city)) {
+        cities.add(city);
+      }
+      lineSc.next();
+      city = lineSc.next();
+      if (! cities.contains(city)) {
+        cities.add(city);
+      }
+    }
+    // System.out.println(cities);
   }
 
 }
