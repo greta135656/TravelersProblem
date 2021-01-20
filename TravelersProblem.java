34c1ee3bac7a64ab901d62a7e8e0ced0a2e6fd7b
Elizabeth Stansberry <estansberry20@stuy.edu>
2021-01-19 19:15:51 -0700

    Starting

--- TravelersProblem.java
+++ TravelersProblem.java
@@ -1,9 +1,14 @@
-import java.util.Random;
+import java.util.*;
+import java.lang.*;
 
 public class TravelersProblem {
 
   public static void main(String[] args) {
-
+    Scanner linescan = new Scanner(System.in);
+    String inputtext;
+    String wordtext;
+    while(linescan.hasNextLine()){
+    }
   }
 
 }
